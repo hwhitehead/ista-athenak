b76aad7921428b3cb219bdf047e7f94aeb43bf92
hwhitehead <hwhiteead25@gmail.com>
2026-09-19 12:27:58 +0200

    further cavity dens decrease

--- src/pgen/ista/nbody.cpp
+++ src/pgen/ista/nbody.cpp
@@ -104,7 +104,7 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
       const Real r = Kokkos::sqrt(r_sqr);
 
       // set density by cavity kernel
-      const Real cavity_fac = 1e-5 + (1.0 - 1e-5) * Kokkos::exp(-Kokkos::pow((r_cavity / r), 4.0)); 
+      const Real cavity_fac = 1e-7 + (1.0 - 1e-7) * Kokkos::exp(-Kokkos::pow((r_cavity / r), 4.0)); 
       Real rho = rho0 * cavity_fac; // flat nu -> flat rho outside cavity
       if (alpha != 0.0) rho *= Kokkos::pow(r, -1.5); // inhomo nu, update powerlaw
 
