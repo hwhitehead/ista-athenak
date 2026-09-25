fdb710884200731684cd1c0a58b4c48398fb007b
hwhitehead <hwhiteead25@gmail.com>
2026-09-25 10:18:43 +0200

    err in ptr path to mbp

--- src/nbody/nbody_tasks.cpp
+++ src/nbody/nbody_tasks.cpp
@@ -68,8 +68,8 @@ TaskStatus NBody::ReduceParentMesh(Driver *pdrive, int stage) {
   // Step 4: Collect updates across mb_packs on this rank 
   for (int mbp_id = 0; mbp_id < pmy_pack->pmesh->nmb_packs_thisrank; mbp_id++) {
     // enforce device->host sync for MeshBlockPack
-    pmy_pack->pmesh->pmy_pack[mbp_id].pnbody->delta_this_pack.template modify<DeviceMemSpace>(); // TODO: shift this to source term?
-    pmy_pack->pmesh->pmy_pack[mbp_id].pnbody->delta_this_pack.template sync<HostMemSpace>();
+    pmy_pack->pmesh->pmb_pack[mbp_id].pnbody->delta_this_pack.template modify<DeviceMemSpace>(); // TODO: shift this to source term?
+    pmy_pack->pmesh->pmb_pack[mbp_id].pnbody->delta_this_pack.template sync<HostMemSpace>();
     // sum device-synchronised pack data into mesh data
     for (int n = 0; n < num_nbody; n++) {
       for (int i = 0; i < NVAR_BACK; i++) {
@@ -214,7 +214,7 @@ TaskStatus NBody::Scatter(Driver *pdrive, int stage) {
   // Step 4: reset registers and force update of device state on ALL mb_packs
   for (int mbp_id = 0; mbp_id < pmy_pack->pmesh->nmb_packs_thisrank; mbp_id++) {
     // wipe source-accesible backreaction register for next step
-    Kokkoss::deep_copy(pmy_pack->pmesh->pmb_pack[mbp_id].pnbody->delta_this_pack.view_host(), 0.0);
+    Kokkos::deep_copy(pmy_pack->pmesh->pmb_pack[mbp_id].pnbody->delta_this_pack.view_host(), 0.0);
     // sync updates to device
     pmy_pack->pmesh->pmb_pack[mbp_id].pnbody->nbody_data.template modify<HostMemSpace>();
     pmy_pack->pmesh->pmb_pack[mbp_id].pnbody->delta_this_pack.template modify<HostMemSpace>();
